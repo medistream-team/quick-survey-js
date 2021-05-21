764e0ecf67a093e26e3eb59706215f2398e2604b
moonimooni <munhw1408@gmail.com>
2021-05-21 19:55:59 +0900

    Add : 몽고DB 스키마 작성

---
+++ BACK-END/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json({ strict: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+module.exports.handler = serverless(app);
