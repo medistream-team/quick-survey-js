2d7d2ad0bfb171d9436a7089e495bc01a6f30fa5
Jessica Lee <to.jessywlee@gmail.com>
2021-05-23 01:34:37 +0900

    frontend first commit

---
+++ FRONT-END/src/components/PollTitles.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="pollTitleContainer">
+    <div class="pollTitle">{{ title }}</div>
+    <div class="pollSubtitle">{{ subtitle }}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "PollTitle",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.pollTitleContainer {
+  .pollTitle {
+    font-size: 24px;
+    overflow-wrap: break-word;
+  }
+
+  .pollSubtitle {
+    font-size: 14px;
+  }
+}
+</style>
---
+++ FRONT-END/src/components/Scale.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="scaleContainer" :style="fillResult(scaleSize)">
+    <div class="scaleResult">
+      <li
+        @click="handleScaleClick"
+        class="eachScale"
+        :class="{ clicked: isClicked }"
+      >
+        {{ scaleText }}
+        <span v-if="showResult">{{ getResult(scaleSize) }}</span>
+      </li>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Scale",
+  props: {
+    totalCount: {
+      type: Number,
+      required: true,
+    },
+    scaleId: {
+      type: String,
+      required: true,
+    },
+    scaleText: {
+      type: String,
+      required: true,
+    },
+    scaleCount: {
+      type: Number,
+      required: true,
+    },
+    showResult: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isClicked: false,
+    };
+  },
+  computed: {
+    scaleSize() {
+      return (this.scaleCount / this.totalCount) * 100;
+    },
+  },
+  methods: {
+    handleScaleClick() {
+      if (this.showResult) {
+        this.isClicked = !this.isClicked;
+      }
+    },
+    fillResult(num) {
+      if (this.showResult) {
+        return {
+          background: `linear-gradient(to top, #ddd ${num}%, #fff ${num}%)`,
+        };
+      }
+    },
+    getResult(num) {
+      if (this.showResult) {
+        return `${num}%`;
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.scaleContainer {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2px 7px;
+  width: 50px;
+  height: 50px;
+  border: 1px solid #d8d8d8;
+  border-radius: 50%;
+  list-style-type: none;
+  .eachScale {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    span {
+      font-size: 12px;
+    }
+  }
+  .clicked {
+    border: 1px solid #2275ff;
+  }
+}
+</style>
