82d794217242902ef1bfaaf04f6d1a349d08715d
moonimooni <munhw1408@gmail.com>
2021-05-25 19:21:00 +0900

    Add : Backend 파일 정리 완료

--- FRONT-END/src/config.js
+++
@@ -1,5 +0,0 @@
-const API = "https://n70xgo22r2.execute-api.ap-northeast-2.amazonaws.com";
-
-export const USER_POLL_API = `${API}/dev/survey`;
-export const ADMIN_POLL_API = `${API}/dev/admin/survey`;
-export const TOKEN = '';
