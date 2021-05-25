20b08d180ed17299c62b076bb5e3bb2e43f746df
moonimooni <munhw1408@gmail.com>
2021-05-25 10:20:52 +0900

    Add : PATCH admin/survey/:surveyId/status

--- BACK-END/routers/admin.js
+++ BACK-END/routers/admin.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const adminController = require("../controllers/admin");
 
 router.post("/survey", adminController.createSurvey);
+router.patch("/survey/:surveyId/status", adminController.patchSurvey);
 
 module.exports = router;
