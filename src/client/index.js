adf57e6208be37c8e3ae63d1e43121bb4db8a7cc
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-05-30 03:08:44 +0900

    패키지로 사용할 수 있도록 한다

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,12 @@
-import axios from "axios";
-import VueAxios from "vue-axios";
-import Vuetify from "vuetify";
 import PollAdmin from "./views/PollAdmin.vue";
-require("./vuetify-reset.css");
+import Poll from "./views/Poll.vue";
 
 const Plugin = {
   // eslint-disable-next-line no-unused-vars
   install(Vue, options = {}) {
     Vue.config.productionTip = false;
-    Vue.use(VueAxios, axios);
-    Vue.use(Vuetify);
-    Vue.component("medi-poll-admin", PollAdmin);
+    Vue.component("open-survey-creator", PollAdmin);
+    Vue.component("open-survey", Poll);
   },
 };
 
