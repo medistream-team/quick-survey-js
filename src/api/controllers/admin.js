6af5a2e5f198a0cbf3e8f2fa576d1b0c6e1fb1d5
moonimooni <munhw1408@gmail.com>
2021-06-01 15:46:57 +0900

    임시 로깅

--- src/api/controllers/admin.js
+++ src/api/controllers/admin.js
@@ -31,13 +31,31 @@ const createSurvey = async (req, res, next) => {
 
 const patchSurvey = async (req, res, next) => {
   try {
-    const creatorKey = req.header("authorization");
-    const surveyId = req.params.surveyId;
-    const { isActive } = req.body;
+    console.log("header에 담긴 키: ", creatorKey);
+    if (!creatorKey) {
+      throw newError("unauthorized", 401);
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(surveyId)) {
+      throw newError("invalid object id", 400);
+    }
+
+    if (!("isActive" in req.body)) {
+      throw newError("key error", 400);
+    }
+
+    if (typeof isActive !== "boolean") {
+      throw newError("value error", 400);
+    }
+
+    if (!(await Survey.exists({ _id: surveyId }))) {
+      throw newError("survey not found", 404);
+    }
 
     const survey = await Survey.findById(surveyId).select(
       "isActive creatorKey"
     );
+    console.log("설문의 생성자 key: ", survey.creatorKey);
     if (survey.creatorKey !== creatorKey) {
       const error = customError.unauthorizedError();
       throw error;
