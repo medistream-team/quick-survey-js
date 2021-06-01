2896527185d1d868356bee711ef5d3f56db00504
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-06-01 18:26:26 +0900

    투표 과정을 event 기반으로 변경한다

--- src/client/views/InvolvePoll.vue
+++ src/client/views/InvolvePoll.vue
@@ -1,5 +1,11 @@
 <template>
-  <Poll />
+  <Poll
+    :surveyId="'60b5d70dfb89aa54854713f6'"
+    :userKey="'thu'"
+    @vote-success="voteSucceed"
+    @already-voted="alreadyVoted"
+    @poll-closed="pollClosed"
+  />
 </template>
 
 <script>
@@ -8,5 +14,17 @@ import Poll from "../components/Poll";
 export default {
   name: "InvolvePoll",
   components: { Poll },
+  methods: {
+    voteSucceed(surveyId) {
+      this.$router.push(`/poll/results/${surveyId}`);
+    },
+    alreadyVoted(surveyId) {
+      alert("이미 참여한 투표입니다.");
+      this.$router.push(`/poll/results/${surveyId}`);
+    },
+    pollClosed(surveyId) {
+      console.log("poll-closed", surveyId);
+    },
+  },
 };
 </script>
