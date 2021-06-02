f2b85435c34f8b27865e1a0cf3feaed506c9610d
Jessica Lee <to.jessywlee@gmail.com>
2021-06-03 00:19:02 +0900

    README 추가, Add Choices 로직 수정, vuetify  아이콘 삭제

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,15 @@
-import SurveyAdmin from "./views/SurveyAdmin.vue";
-import Survey from "./views/Survey.vue";
-import SurveyResults from "./views/SurveyResults.vue";
+import SurveyAdmin from "./components/SurveyAdmin.vue";
+import Survey from "./components/Survey.vue";
+import SurveyResults from "./components/SurveyResults.vue";
 import "vuetify/dist/vuetify.min.css";
 
 const Plugin = {
   // eslint-disable-next-line no-unused-vars
   install(Vue, options = {}) {
     Vue.config.productionTip = false;
-    Vue.component("open-survey-creator", SurveyAdmin);
-    Vue.component("open-survey", Survey);
-    Vue.component("open-survey-results", SurveyResults);
+    Vue.component("quick-survey-admin", SurveyAdmin);
+    Vue.component("quick-survey", Survey);
+    Vue.component("quick-survey-results", SurveyResults);
   },
 };
 
