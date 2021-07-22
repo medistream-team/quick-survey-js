684b4c43cd3be1e4c8f3877fc97e12925f02bac9
moonimooni <munhw1408@gmail.com>
2021-07-23 02:32:59 +0900

    Refactor : PATCH survey status 엔드포인트 함수 분리

--- src/api/controllers/admin.js
+++ src/api/controllers/admin.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const Survey = require("../models/surveys/schema");
 const surveyService = require("../services/survey");
 const userService = require("../services/user");
-
-const { customError } = require("../utils/custom-errors");
 
 const createSurvey = async (req, res, next) => {
   try {
@@ -31,43 +28,15 @@ const createSurvey = async (req, res, next) => {
 
 const patchSurvey = async (req, res, next) => {
   try {
-    console.log("header에 담긴 키: ", creatorKey);
-    if (!creatorKey) {
-      throw newError("unauthorized", 401);
-    }
+    const user = req.user;
+    const { surveyId } = req.params;
+    const { isActive } = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(surveyId)) {
-      throw newError("invalid object id", 400);
-    }
-
-    if (!("isActive" in req.body)) {
-      throw newError("key error", 400);
-    }
-
-    if (typeof isActive !== "boolean") {
-      throw newError("value error", 400);
-    }
-
-    if (!(await Survey.exists({ _id: surveyId }))) {
-      throw newError("survey not found", 404);
-    }
-
-    const survey = await Survey.findById(surveyId).select(
-      "isActive creatorKey"
-    );
-    console.log("설문의 생성자 key: ", survey.creatorKey);
-    if (survey.creatorKey !== creatorKey) {
-      const error = customError.unauthorizedError();
-      throw error;
-    }
-    survey.isActive = isActive;
-    survey.save();
+    await surveyService.patchSurvey(user, surveyId, isActive);
 
     return res.status(200).json({ message: "success" });
-  } catch (error) {
-    return res
-      .status(error.code ? error.code : 400)
-      .json({ message: error.message });
+  } catch (err) {
+    return next(err);
   }
 };
 
